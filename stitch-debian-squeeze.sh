e4ea3dd2ebcfe2cb704243f8a70b99be494b6891
tdett <prinzipiell@gmail.com>
2012-06-03 14:09:56 +0300

    Revert back to HFOV 10 because of error.

--- stitch-debian-squeeze.sh
+++ stitch-debian-squeeze.sh
@@ -3,7 +3,7 @@ Xvfb :0 &
 export DISPLAY=:0
 ls -l CIM*.JPG | awk '{printf("%s ", $9)}' > input.txt
 autopano-c-complete -o stitched.pto `cat input.txt`
-autooptimiser -v 9500 -a -l -s -o optimised.pto stitched.pto
+autooptimiser -v 10 -a -l -s -o optimised.pto stitched.pto
 pto2mk -o final -p final optimised.pto
 make -f final
 mogrify -format jpg -quality 100 final.tif
